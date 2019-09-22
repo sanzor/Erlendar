f4ee1125e910ddd3b915cab6c4b9e28e9f8a17ca
sanzor <adriansan_93@yahoo.com>
2019-09-22 17:48:03 +0300

    added event module

--- event.erl
+++ event.erl
@@ -1,11 +1,17 @@
 -module(event).
 -compile([debug_info]).
+-export([event/1]).
+
+-record(state,{server,
+               name="",
+               to_go=0}).
 
 
 
-
-event(Delay)->
+event(S=#state{server=Server})->
     receive
-        {cancel}->exit(cancelled)
-    after Delay
-       exit(ok)
+        {Server,Ref,cancel}->
+            Server ! {Ref,ok}
+    after S#state.to_go*1000 ->
+        Server ! {done,S#state.name}
+end.
