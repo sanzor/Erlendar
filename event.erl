ea3a7457a36985534a182a0c7b495f1aa66c199e
sanzor <adriansan_93@yahoo.com>
2019-10-04 00:27:42 +0300

    event module

--- event.erl
+++ event.erl
@@ -1,17 +1,41 @@
--module(event).
+*-module(event).
 -compile([debug_info]).
 -export([loop/1]).
 
--record(state,{server,
-               name="",
-               to_go=0}).
+-record(state,
+ {server,
+  name="",
+  to_go=0
+}).
 
+normalize(N)->
+        Limit = 49*24*60*60,
+        [N rem Limit | lists:duplicate(N div Limit,Limit)]
 
+start(EventName,Delay)->
+    spawn(?MODULE,init,[self(),EventName,Delay]).
 
-loop(S=#state{server=Server})->
+start_link(EventName,Delay)->
+        spawn_link(?MODULE,init,[self(),EventName,Delay]).
+
+cancel(EventName)->
+    
+init(Server,EventName,Delay)->
+    loop(#state{
+                server=Server,
+                name=EventName,
+                to_go=normalize(Delay)}
+        ).
+
+loop(S=#state{server=Server,to_go=[T|Next]})->
     receive
         {Server,Ref,cancel}->
-            Server ! {Ref,ok}
-    after S#state.to_go*1000 ->
-        Server ! {done,S#state.name}
+            Server ! {Ref , ok}
+    after T*1000->
+        if Next =:= [] ->
+            Server ! {done,S#state.name};
+           Next =/= [] ->
+               loop(S#state{to_go=Next})
 end.
+
+
