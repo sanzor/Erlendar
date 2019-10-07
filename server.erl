97ed5318d2031feeb55729da25e3320dfa4c24fe
sanzor <adriansan_93@yahoo.com>
2019-10-08 00:23:33 +0300

    finishing event server

--- server.erl
+++ server.erl
@@ -5,7 +5,9 @@
                 description="",
                 pid,
                 timeout={{1970,1,1}{0,0,0}}
+}
 ).
+
 
 loop(State)->
     receive 
@@ -32,53 +34,51 @@ loop(State)->
                     Pid ! {MsgRef,{error,bad_timeout}},
                     loop(State)
             end;
-
-
-        {Pid,MsgRef,{cancel,Name}}->
-            
-            Events=case orddict:find(Name,State#state.events) of ->
+        {Pid,MsgRef,{cancel,Name}} ->
+            Events=case orddict:find(Name,State#state.events) of 
                     {ok,Event} -> 
                         cancel(Event#event.pid),
                         orddict:erase(Name,State#state.events);
                     error->
                         State#state.events
-                end,
+                   end,
             Pid ! {MsgRef,ok},
-            loop(State#{events=Events}).
+            loop(State#{events=Events});
                 
-        {done,Name}->
+        {done,Name} ->
             case orddict:find(Name,State#state.events) of
                 {ok,E}->
                     send_to_clients({done,E#event.name,E#event.description},State#state.events),
                     NewEvents=orddict:erase(E,State#state.events),
-                    loop(State#state{events=NewEvents}).
+                    loop(State#state{events=NewEvents});
                 error->
                     loop(State)
-            end.
-        shutdown->
+            end;
+        shutdown->exit(shutdown);
         {'DOWN',Ref,process,_Pid,_Reason}->
-        code_change->
+            loop(State#state{clients=orddict:erase(Ref,State#state.clients)});
+
+        code_change->?MODULE:loop(State);
         Unknown->
             io:format("unkown message"),
             loop(State).
 
 
-send_to_clients(Msg,ClientId)->
-    orddict:
+send_to_clients(Msg,ClientDict)->
+        orddict:map(fun(_Ref, Pid) -> Pid ! Msg end, ClientDict).
 valid_datetime({Date,Time})->
     try 
         calendar:valid_date(Date) andalso valid_time(Time)
     catch
-        error:function_clause->false;
-            
+        error:function_clause->false 
     end;
 valid_datetime(_)->false.
 
 valid_time({H,M,S})->valid_time(H,M,S).
-valid_time(H,M,S) when  H>=0 && H<24,
-                        M>=0 && M<60,
-                        S>=0 && S<60 ->true;
+valid_time(H,M,S) when  H>=0 , H<24,
+                        M>=0 , M<60,
+                        S>=0 , S<60 ->true;
 valid_time(_,_,_)->false;
 init()->
-    loop(State#state{events=orddict:new(),clients=orddict:new()}).
+    loop(#state{events=orddict:new(),clients=orddict:new()}).
             
